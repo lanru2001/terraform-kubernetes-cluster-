bb5f1f17573347e0f6220b071c0a7a515a0de54f
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-03-19 17:25:45 -0500

    Update app_service.tf

--- kubernetes/app_service.tf
+++ kubernetes/app_service.tf
@@ -3,6 +3,9 @@ resource "kubernetes_service" "wordpress" {
     name = "${var.deployment_name}-service"
     namespace = kubernetes_namespace.namespace.metadata[0].name
     labels = var.labels
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-type" = "nlb-ip"
+    }
   }
   spec {
     selector = var.labels
