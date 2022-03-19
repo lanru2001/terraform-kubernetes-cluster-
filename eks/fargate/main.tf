2f5e28a89715889aa444509b4b18cf722fa44df3
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-03-19 17:13:34 -0500

    Update main.tf

--- eks/fargate/main.tf
+++ eks/fargate/main.tf
@@ -12,3 +12,31 @@ resource "aws_eks_fargate_profile" "main" {
     namespace = var.kubernetes_namespace
   }
 }
+
+resource "aws_iam_role" "fargate_pod_execution_role" {
+  name                  = "eks-fargate-pod-execution-role"
+  force_detach_policies = true
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "eks.amazonaws.com",
+          "eks-fargate-pods.amazonaws.com"
+          ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_pod_execution_role.name
+}
